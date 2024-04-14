667a1396b590fc4f52dd95c5189eb67e7715aa47
ArturSouza0 <artur.souza955@gmail.com>
2024-04-14 11:38:53 -0300

    angular-memoteca

---
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+
+@Component({
+  selector: 'app-listar-pensamento',
+  templateUrl: './listar-pensamento.component.html',
+  styleUrl: './listar-pensamento.component.css',
+})
+export class ListarPensamentoComponent implements OnInit {
+  listarPensamentos: Pensamento[] = [];
+
+  constructor(private service: PensamentoService) {}
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((listarPensamentos) => {
+      this.listarPensamentos = listarPensamentos;
+    });
+  }
+}
